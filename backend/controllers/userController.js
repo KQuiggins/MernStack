5e6f30706d61f408aed23e7eaf8952b8d966ae07
KQuiggins <kashfamilyvalues@gmail.com>
2023-05-14 12:58:38 -0400

    setup json web token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from '../middleware/asyncHandler.js';
 import User from '../models/userModel.js';
+import jwt from 'jsonwebtoken';
 
 
 
@@ -7,6 +8,33 @@ import User from '../models/userModel.js';
 // @route  POST /api/users/login
 // @access Public
 const authUser = asyncHandler(async (req, res) => {
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+
+        // set jwt as HttpOnly cookie
+        res.cookie('jwt', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== 'development',
+            sameSite: 'strict',
+            maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
+        });
+
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        })
+    } else {
+        res.status(401);
+        throw new Error('Invalid email or password');
+    }
+
     res.send('auth user')
 });
 
@@ -63,6 +91,7 @@ const deleteUser = asyncHandler(async (req, res) => {
 // @route  PUT /api/users/:id
 // @access Private/Admin
 const updateUser = asyncHandler(async (req, res) => {
+
     res.send('update user')
 });
 
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
@@ -10,6 +11,10 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 const port = process.env.PORT || 3000;
 connectDB();
 const app = express();
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 app.get('/', (req, res) => {
