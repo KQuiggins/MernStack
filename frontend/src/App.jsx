a4139046747c9ab9cacc8218517a5654d4f84d03
KQuiggins <kashfamilyvalues@gmail.com>
2023-05-16 21:23:33 -0400

    add toastify and FormContainer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import Header from './components/Header';
 import { Container } from 'react-bootstrap';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Footer from './components/Footer';
 import { Outlet } from 'react-router-dom';
 
@@ -13,6 +15,7 @@ const App = () => {
             </Container>
         </main>
       <Footer />
+      <ToastContainer />
     </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,38 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import HomeScreen from './screens/HomeScreen'
-import { Provider } from 'react-redux'
-import store from './store'
-import ProductScreen from './screens/ProductScreen'
-import CartScreen from './screens/CartScreen'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HomeScreen from "./screens/HomeScreen";
+import { Provider } from "react-redux";
+import store from "./store";
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-  RouterProvider
+  RouterProvider,
+} from "react-router-dom";
 
- } from 'react-router-dom';
-
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 // import './assets/styles/bootstrap.custom.css'
-import App from './App.jsx'
-import './assets/styles/index.css'
+import App from "./App.jsx";
+import "./assets/styles/index.css";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App />} >
+    <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
       <Route path="/cart" element={<CartScreen />} />
-    </Route>
-
-  )
+      <Route path="/login" element={<LoginScreen />} />
+    </Route>,
+  ),
 );
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
-)
+);
