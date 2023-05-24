8718664de094e229b302adb7bf3c36669a2d1c7a
KQuiggins <kashfamilyvalues@gmail.com>
2023-05-23 22:05:07 -0400

    finished order steps in orderScreen

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,6 +10,7 @@ import RegisterScreen from "./screens/RegisterScreen";
 import ShippingScreen from "./screens/ShippingScreen";
 import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import OrderScreen from "./screens/OrderScreen";
 import PrivateRoute from "./components/PrivateRoute";
 import {
   createBrowserRouter,
@@ -37,6 +38,7 @@ const router = createBrowserRouter(
         <Route path="/shipping" element={<ShippingScreen />} />
         <Route path="/payment" element={<PaymentScreen />} />
         <Route path="/placeorder" element={<PlaceOrderScreen />} />
+        <Route path="/order/:id" element={<OrderScreen />} />
       </Route>
 
     </Route>,
