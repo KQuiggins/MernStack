15a4fa131114d52a63e47c045d8d8dd0214acf5c
KQuiggins <kashfamilyvalues@gmail.com>
2023-05-29 07:47:57 -0400

    productEditScreen, added provided tags to slice

--- frontend/src/screens/admin/ProductEditScreen.jsx
+++ frontend/src/screens/admin/ProductEditScreen.jsx
@@ -1,9 +1,171 @@
-
+import { useState, useEffect } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import Message from "../../components/Message";
+import Loader from "../../components/Loader";
+import { VscArrowLeft } from "react-icons/vsc";
+import FormContainer from "../../components/FormContainer";
+import {
+  useGetProductDetailsQuery,
+  useUpdateProductMutation,
+} from "../../slices/productsApiSlice";
 
 const ProductEditScreen = () => {
-  return (
-    <div>ProductEditScreen</div>
-  )
-}
+  const { id: productId } = useParams();
 
-export default ProductEditScreen
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [image, setImage] = useState("");
+  const [brand, setBrand] = useState("");
+  const [category, setCategory] = useState("");
+  const [countInStock, setCountInStock] = useState(0);
+  const [description, setDescription] = useState("");
+
+  const {
+    data: product,
+    isLoading,
+    error,
+    refetch,
+  } = useGetProductDetailsQuery(productId);
+
+  const [updateProduct, { isLoading: loadingUpdate }] =
+    useUpdateProductMutation();
+
+  useEffect(() => {
+    if (product) {
+      setName(product.name);
+      setPrice(product.price);
+      setImage(product.image);
+      setBrand(product.brand);
+      setCategory(product.category);
+      setCountInStock(product.countInStock);
+      setDescription(product.description);
+    }
+  }, [product]);
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        const updatedProduct = {
+            productId,
+            name,
+            price,
+            image,
+            brand,
+            category,
+            countInStock,
+            description,
+        };
+
+        const result = await updateProduct(updatedProduct);
+        if (result.error) {
+            toast.error(result.error);
+        } else {
+            toast.success("Product Updated");
+            navigate("/admin/productlist");
+        }
+    };
+
+  const navigate = useNavigate();
+  return (
+    <>
+      <Link to="/admin/productlist" className="btn btn-dark my-3">
+        <VscArrowLeft className="mx-3" />
+      </Link>
+      <FormContainer>
+        <h1>Edit Product</h1>
+        {loadingUpdate && <Loader />}
+
+        {isLoading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="danger">{error}</Message>
+        ) : (
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId="name" className="my-2">
+              <Form.Label>Enter Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="price" className="my-2">
+              <Form.Label>Enter Price</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Enter price"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            {/* Image input placeholder */}
+
+            <Form.Group controlId="brand" className="my-2">
+              <Form.Label>Enter Brand</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter brand"
+                value={brand}
+                onChange={(e) => setBrand(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="countInStock" className="my-2">
+              <Form.Label>Count In Stock</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Enter stock count"
+                value={countInStock}
+                onChange={(e) => setCountInStock(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="category" className="my-2">
+              <Form.Label>Enter Category</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Category"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="description" className="my-2">
+              <Form.Label>Enter Description</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={5}
+                placeholder="Description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Button
+              type="submit"
+              variant="outline-dark"
+              className="my-2"
+              style={{
+                backgroundColor: "white",
+                border: "1px solid black",
+                color: "black",
+                transition: "background-color 0.3s ease",
+              }}
+              onMouseEnter={(e) => (e.target.style.backgroundColor = "gray")}
+              onMouseLeave={(e) => (e.target.style.backgroundColor = "white")}
+            >
+              Update
+            </Button>
+          </Form>
+        )}
+      </FormContainer>
+    </>
+  );
+};
+
+export default ProductEditScreen;
