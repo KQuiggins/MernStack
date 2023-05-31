591efaa46940ec1616828e90304442a5bca826bd
KQuiggins <kashfamilyvalues@gmail.com>
2023-05-31 15:47:00 -0400

    user reviews complete

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -102,8 +102,30 @@ const createProductReview = asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
 
     if (product) {
-        await Product.deleteOne({_id: product._id});
-        res.status(200).json({message: 'Product deleted successfully'});
+
+            const alreadyReviewed = product.reviews.find(review => review.user.toString() === req.user._id.toString());
+
+            if (alreadyReviewed) {
+                res.status(400);
+                throw new Error('Product already reviewed');
+            }
+
+            const review = {
+                name: req.user.name,
+                rating: Number(rating),
+                comment,
+                user: req.user._id
+            };
+
+            product.reviews.push(review);
+
+            product.numReviews = product.reviews.length;
+
+            product.rating = product.reviews.reduce((acc, item) => item.rating + acc, 0) / product.reviews.length;
+
+            await product.save();
+
+            res.status(201).json({message: 'Review added'});
 
     } else {
         res.status(404);
@@ -117,5 +139,6 @@ export {
     getProductById,
     createProduct,
     updateProduct,
-    deleteProduct
+    deleteProduct,
+    createProductReview
 };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,7 +5,8 @@ import {
     getProductById,
     createProduct,
     updateProduct,
-    deleteProduct
+    deleteProduct,
+    createProductReview
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
@@ -19,6 +20,7 @@ router
     .get(getProductById)
     .put(protect, admin, updateProduct)
     .delete(protect, admin, deleteProduct);
+router.route('/:id/reviews').post(protect, createProductReview);
 
 
 export default router;
