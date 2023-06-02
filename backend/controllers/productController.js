d864dbd18964c18b45dcb23a014f8e01c33f24de
KQuiggins <kashfamilyvalues@gmail.com>
2023-06-02 07:34:18 -0400

    set environment variable for pagination limit

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -7,7 +7,7 @@ import Product from '../models/productModel.js';
 // @route  GET /api/products
 // @access Public
 const getProducts = asyncHandler(async (req, res) => {
-    const pageSize = 8;
+    const pageSize = process.env.PAGINATION_LIMIT || 8;
     const page = Number(req.query.pageNumber) || 1;
 
     const keyword = req.query.keyword ? {name: { $regex: req.query.keyword, $options: 'i' }} : {};
